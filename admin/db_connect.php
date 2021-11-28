88a5e8718c9d2be8e58faabbd885192128c027cc
zlfahmi <z.fahmi312@gmail.com>
2021-11-28 09:58:38 +0800

    Change database

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','SOS')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','sos-edited')or die("Could not connect to mysql".mysqli_error($con));

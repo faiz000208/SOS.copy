bd60c2e69200e2566a52c9094a56c63bbbb82982
faiz000208 <2021112699@student.uitm.edu.my>
2021-12-07 00:49:10 +0800

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','sos-edited')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','sos-edited-new')or die("Could not connect to mysql".mysqli_error($con));
